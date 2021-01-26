4af08d71a9b3c47f9cbc03c3f9bad63615f9ae1b
Yauheni Kiryienkau <ya@redstream.by>
2021-01-26 11:29:34 +0300

    Update install-phpenv.sh

--- install-phpenv.sh
+++ install-phpenv.sh
@@ -4,10 +4,10 @@ dpkg-reconfigure tzdata && \
 apt update && \
 apt upgrade -y && \
 add-apt-repository ppa:ondrej/php -y && \
-apt install -y software-properties-common apt-utils rsync mc htop dialog nginx curl certbot python3-certbot-nginx && \
+apt install -y software-properties-common apt-utils rsync mc htop dialog nginx curl && \
 apt install -y php7.4-imagick php7.4-fpm php7.4-curl php7.4-xml php7.4-zip php7.4-mbstring php7.4-mysql && \
 apt install -y mysql-server && \
-echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'yasooshae'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql && \
+echo "CREATE USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'yasooshae'; GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql && \
 curl -sL https://deb.nodesource.com/setup_lts.x | bash && \
 apt install -y nodejs && \
 npm i -g pm2 && \
